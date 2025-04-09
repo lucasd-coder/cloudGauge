fc7eb94261c50a12d31e4a2602aec51074097fd3
lucas <lucasdasilva68@yahoo.com>
2025-04-09 17:01:41 -0300

    feat: Add pulseReceiver initial project structure
    This commit introduces the basic structure for the pulseReceiver project with:
    - Basic application setup (cmd/app)
    - Configuration management
    - Go module initialization

---
+++ pulseReceiver/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import "time"
+
+var cfg *Config
+
+type (
+	Config struct {
+		App         `yaml:"app"`
+		HTTP        `yaml:"http"`
+		Log         `yaml:"logger"`
+		Integration `yaml:"integration"`
+	}
+
+	App struct {
+		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+
+	Log struct {
+		Level        string `env-required:"true" yaml:"log-level"   env:"LOG_LEVEL"`
+		ReportCaller bool   `yaml:"report-caller" default:"false"`
+	}
+
+	HTTP struct {
+		Port    string        `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+		Timeout time.Duration `env-required:"true" yaml:"timeout"`
+	}
+
+	Integration struct {
+		OpenTelemetry `env-required:"true" yaml:"otlp"`
+	}
+
+	OpenTelemetry struct {
+		URL      string        `env-required:"true" yaml:"url" env:"OTEL_EXPORTER_OTLP_ENDPOINT"`
+		Protocol string        `env-required:"true" yaml:"protocol" env:"OTEL_EXPORTER_OTLP_PROTOCOL"`
+		Timeout  time.Duration `env-required:"true" yaml:"timeout" env:"OTEL_EXPORTER_OTLP_TIMEOUT"`
+	}
+)
+
+func ExportConfig(config *Config) {
+	cfg = config
+}
+
+func GetConfig() *Config {
+	return cfg
+}
